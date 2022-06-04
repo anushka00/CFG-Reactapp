8d031a0255e533dae9b37d6f1601c4f5f8d81f58
Aaryan Mehta <aaryan.mehta2506@gmail.com>
2022-06-04 18:17:31 +0530

    Completed the First Home Page

---
+++ client/src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Navbar } from './Navbar'
+export const About = () => {
+  return (
+    <div>
+      <Navbar/>
+          <h3>Welcome to About Page</h3>
+    </div>
+  )
+}
