55c4121a479f35a3c23c25f9649128123566c7cb
Aaryan Mehta <aaryan.mehta2506@gmail.com>
2022-06-04 23:13:34 +0530

    checks

--- client/src/components/Logpara.js
+++ client/src/components/Logpara.js
@@ -1,54 +1,40 @@
 import React from "react";
 import { Navbar } from "./Navbar";
+import './lpara.css'
 
 export const Logpara = () => {
 
  return(
      <>
-    <Navbar/>
-    <form>
-         <div className="form-body">
-        <div className="row">
-            <div className="form-holder">
-                <div className="form-content">
-                <div class="form-items">
-     <h1>LOGGED IN AS A PARAMEDIC </h1>
-     <label>PATIENT ID </label>
-     <input name="number"  />
-     <div className="col-md-6 mb-4">
-
+     <form>
+     <div className="container">
+         <h2 className="main_heading" ><b>LOGGED IN AS A PARAMEDIC </b></h2>
+         <p>PATIENT ID</p> 
+         <input type="number" name="id"/>
+         <p>Location</p>
+         <div>
                     <select className="select">
-                      <option value="1" >Location where Service is Required</option>
-                      <option value="2">Delhi</option>
+                      <option value="1">Delhi</option>
+                      <option value="2" >Bangalore</option>
                       <option value="3" >Chennai</option>
-                      <option value="4" >Bombay</option>
                     </select>
-
-                  </div>
-
-                  <div className="col-md-6 mb-4">
-
+        </div>
+         <p>Services Offered</p>
+         <div>
                     <select className="select">
                       <option value="1" >Types of Services </option>
                       <option value="2">Clinic Consultation</option>
                       <option value="3" >Surgery</option>
                       <option value="3" >Medicinal Items</option>
                       <option value="4">Vacant Beds/Rooms</option>
-
                     </select>
-                  </div>
-                  
-                  <button type="submit" label="req" className="btn btn-success">Create a Request</button>
-                  
-                  <button type="button" label="status" className="btn btn-outline-info">Check the Status</button>
+</div>
+         <button type="submit" label="req" className="btn btn-success">Create a Request</button>
+         <button type="button" label="status" className="btn btn-outline-info">Check the Status</button>
                        
-                    </div>
-                </div>
-            </div>
-        </div> 
-        </div>
-        </form>
 
+         </div>
+     </form>
 </>
  );
 }
