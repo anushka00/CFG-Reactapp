86dec8907d400ba48b1f6f993bb814ab55a9212a
Aynaan <aynaanq@gmail.com>
2022-06-04 22:39:23 +0530

    Updated get routes

--- api/controllers/request.js
+++ api/controllers/request.js
@@ -6,6 +6,7 @@ var nodemailer = require('nodemailer');
 exports.getAllRequest = (req, res, next) => {
     Request
         .find()
+        .select(' _id requested_service patientId request_status service_provider location email')
         .exec()
         .then(request => {
             res.status(200).json({
@@ -47,6 +48,7 @@ exports.getRequestByLocation = (req, res, next) => {
     const  location= req.params.location;
     Request
         .find({location:location})//what is the query of string
+        .select(' _id requested_service patientId request_status service_provider location email')
         .exec()
         .then(request => {
             res.status(201).json(request)
@@ -60,6 +62,7 @@ exports.getRequestByStatus = (req, res, next) => {
     const  status= req.params.request_status;
     Request
         .find({request_status:status})//what is the query of string
+        .select(' _id requested_service patientId request_status service_provider location email')
         .exec()
         .then(request => {
             res.status(201).json(request)
