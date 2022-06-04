ea58c1d0a904a5bf1f460c9ce1af6f9d887022a4
Aaryan Mehta <aaryan.mehta2506@gmail.com>
2022-06-05 00:27:56 +0530

    Added Login of paramedics

--- client/src/components/Logpara.js
+++ client/src/components/Logpara.js
@@ -1,40 +1,45 @@
 import React from "react";
 import { Navbar } from "./Navbar";
 import './lpara.css'
-
 export const Logpara = () => {
 
  return(
      <>
-     <form>
+     <Navbar/>
      <div className="container">
-         <h2 className="main_heading" ><b>LOGGED IN AS A PARAMEDIC </b></h2>
-         <p>PATIENT ID</p> 
-         <input type="number" name="id"/>
-         <p>Location</p>
-         <div>
-                    <select className="select">
-                      <option value="1">Delhi</option>
-                      <option value="2" >Bangalore</option>
-                      <option value="3" >Chennai</option>
-                    </select>
-        </div>
-         <p>Services Offered</p>
-         <div>
-                    <select className="select">
-                      <option value="1" >Types of Services </option>
+       <h3>Request for Service</h3>
+       <div className="form-container">
+         <form>
+           <div className="input-name">
+           <i class="fa-solid fa-user"></i>
+           <input type="number" placeholder="Enter Id" className="name"/>
+           </div>
+
+           <div className="input-name">
+             <select className="country">
+             <option value="1" >Types of Services </option>
                       <option value="2">Clinic Consultation</option>
                       <option value="3" >Surgery</option>
                       <option value="3" >Medicinal Items</option>
                       <option value="4">Vacant Beds/Rooms</option>
-                    </select>
-</div>
-         <button type="submit" label="req" className="btn btn-success">Create a Request</button>
-         <button type="button" label="status" className="btn btn-outline-info">Check the Status</button>
-                       
+             </select>
+           </div>
 
-         </div>
-     </form>
+           <div className="input-name">
+             <select className="country">
+             <option value="1" >Location </option>
+                      <option value="1">Delhi</option>
+                      <option value="2" >Bangalore</option>
+                      <option value="3" >Chennai</option>
+                      <option value="4">Mumbai</option>
+             </select>
+           </div>
+           <button type="submit" label="req"  className="btn btn-danger">Create a Request</button>
+           <button type="button" class="btn btn-success">Check the Status</button>
+          </form>
+          
+       </div>
+     </div>
 </>
  );
 }
